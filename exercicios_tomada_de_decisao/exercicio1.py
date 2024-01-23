b4d1c9b05034d5bbc6f7cafdf063f561fd99caa4
Carme Leticia <carme.leticia@gmail.com>
2024-01-23 19:47:36 -0300

    resolução exercicio tomada de decisão 1

--- exercicios_tomada_de_decisao/exercicio1.py
+++ exercicios_tomada_de_decisao/exercicio1.py
@@ -1,4 +1,12 @@
 '''
 Faça um Programa que peça dois números e imprima o maior deles.
 '''
+numero1 = int(input('Digite seu primeiro número: '))
+
+numero2 = int(input('Digite seu segundo número: '))
+
+if numero1 > numero2:
+    print(f'O número maior é o {numero1}')
+else:
+    print(f'O número maior é o {numero2}')
 
