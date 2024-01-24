d9d4c95671c116da9591bdf43450cb24232694b3
Adriana Ferraresi <119368693+Crisfer-code@users.noreply.github.com>
2024-01-24 12:38:45 -0300

    adicionando enunciado modulo_2_exerc_2

--- exercicios_tomada_de_decisao/exercicio2.py
+++ exercicios_tomada_de_decisao/exercicio2.py
@@ -1,3 +1,10 @@
+'''
+Faça um Programa que pergunte em que turno você estuda. Peça para
+digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom
+Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+'''
+
 while True:
     print("Informe o turno que você estuda:")
     print("M - matutino")
