d535c667421f56eb9a986ff53c84ac07202f1f2c
Laura Perroni <101422414+lauraperroni@users.noreply.github.com>
2024-01-24 08:53:05 -0300

    Update exercicio9.py
    corrigi o meu exercício que não tinha filtro de números negativos

--- exercicios_tomada_de_decisao/exercicio9.py
+++ exercicios_tomada_de_decisao/exercicio9.py
@@ -9,9 +9,13 @@ Responsável: Laura Perroni Quadros da Silva
 
 numero = int(input('Digite um número: '))
 
-while numero != 0 :
-    if numero % 2 == 0 :
-        print('É um número par')
-    else :
-        print('É um número ímpar')
-    numero = int(input('Digite um número: '))
+if numero < 0 :
+    print('Este número não é positivo.')
+else:    
+    while numero != 0 :
+    
+        if numero % 2 == 0 :
+            print('É um número par')
+        else :
+            print('É um número ímpar')
+        numero = int(input('Digite um número: '))
