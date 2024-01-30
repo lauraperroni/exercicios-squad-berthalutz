5d22bef04e5bc6bce1da34d62ef08da27a6d007a
Laura Perroni <lauraperroni2020@gmail.com>
2024-01-30 12:11:33 -0300

    created new folder for weeks 4 assignments

---
+++ Semana 4/exercicios-banco-de-dados/exercicio-gerenciamento-biblioteca/ex1-2-3-4.py
@@ -0,0 +1,48 @@
+'''
+Responsáveis: 
+
+Esse exercício está dividido em 4 partes.
+'''
+
+'''
+1. Criação das Tabelas:
+- Utilizando SQL, crie as tabelas necessárias para armazenar as
+informações do sistema. Certifique-se de definir as chaves primárias e
+estrangeiras conforme apropriado.
+'''
+
+#Escreva o codigo aqui
+
+
+'''
+2. Inserção de Dados:
+- Insira dados de exemplo nas tabelas para simular um ambiente de
+biblioteca. Certifique-se de incluir uma variedade de livros, autores e
+editoras.
+
+'''
+
+#Escreva o codigo aqui
+
+
+
+'''
+3. Consultas SQL:
+Escreva consultas SQL para realizar as seguintes operações:
+- Listar todos os livros disponíveis.
+- Encontrar todos os livros emprestados no momento.
+- Localizar os livros escritos por um autor específico.
+- Verificar o número de cópias disponíveis de um determinado livro.
+- Mostrar os empréstimos em atraso.
+'''
+
+#Escreva o codigo aqui
+
+'''
+4. Atualizações e Exclusões:
+- Escreva consultas SQL para atualizar e excluir registros do banco de
+dados, por exemplo, para marcar um livro como devolvido ou remover
+um autor.
+'''
+
+#Escreva o codigo aqui
---
+++ Semana 4/exercicios-banco-de-dados/exercicio-gerenciamento-mercado/ex1-2-3-4.py
@@ -0,0 +1,45 @@
+'''
+Responsáveis: 
+
+Esse exercício está dividido em 4 partes.
+'''
+
+'''
+1. Criação das Tabelas:
+- Utilizando SQL, crie as tabelas necessárias para armazenar as
+informações do sistema. Defina as chaves primárias e
+estrangeiras conforme apropriado.
+
+'''
+
+#Escreva o codigo aqui
+
+'''
+2. Inserção de Dados:
+- Insira dados de exemplo nas tabelas para simular um ambiente de
+venda de eletrônicos. Certifique-se de incluir uma variedade de
+produtos e clientes
+'''
+
+#Escreva o codigo aqui
+
+'''
+3. Consultas SQL:
+- Escreva consultas SQL para realizar as seguintes operações:
+- Listar todos os produtos em estoque.
+- Encontrar as vendas realizadas por um cliente específico.
+- Calcular o total de vendas por categoria de produto.
+- Identificar os produtos mais vendidos.
+
+'''
+
+#Escreva o codigo aqui
+
+'''
+4. Atualizações e Exclusões:
+- Escreva consultas SQL para atualizar e excluir registros do banco
+de dados, por exemplo, para atualizar a quantidade em estoque
+após uma venda ou remover um cliente.
+'''
+
+#Escreva o codigo aqui
